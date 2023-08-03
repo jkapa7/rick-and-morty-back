b06704013d841f505e1db1b3ae4620883f1e3265
jkapa7 <juandaniel5005@gmail.com>
2023-08-03 14:06:53 -0500

    getDetail controller handler and route completed

--- src/controllers/getDetail.js
+++ src/controllers/getDetail.js
@@ -1,22 +1,18 @@
 const axios = require("axios");
 
-module.exports = async (req, res) => {
-  try {
-    const result = await axios(
-      `https://rickandmortyapi.com/api/character/${detailId}`
-    );
-    const characterDetail = result.data;
-    const character = {
-      id: characterDetail.id,
-      image: characterDetail.image,
-      name: characterDetail.name,
-      gender: characterDetail.gender,
-      species: characterDetail.species,
-      status: characterDetail.status,
-      origin: characterDetail.origin,
-    };
-    res.status(200).json(character);
-  } catch (error) {
-    res.status(500).json("Not found", error);
-  }
+module.exports = async (id) => {
+  const result = await axios(`https://rickandmortyapi.com/api/character/${id}`);
+  const characterDetail = result.data;
+
+  const character = {
+    id: characterDetail.id,
+    image: characterDetail.image,
+    name: characterDetail.name,
+    gender: characterDetail.gender,
+    species: characterDetail.species,
+    status: characterDetail.status,
+    origin: characterDetail.origin,
+  };
+
+  return character;
 };
--- src/handlers/getDetail.js
+++ src/handlers/getDetail.js
@@ -2,7 +2,7 @@ const { getDetail } = require("../controllers");
 const { response } = require("../utils");
 
 module.exports = async (req, res) => {
-  const { detailId } = req.params;
-  const detail = await getDetail();
+  const { id } = req.params;
+  const detail = await getDetail(id);
   response(res, 200, detail);
 };
